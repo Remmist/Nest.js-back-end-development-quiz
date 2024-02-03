780ae22935790678dd48e4cd7d4bac5c8b670ac0
Ivan Illarionov <s25134@pjwstk.edu.pl>
2024-02-03 15:42:50 +0100

    Imported GraphQL, .env file and docker compose
    Imported GraphQL, .env file with database configuration, and docker compose file to create the database in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 
 
 @Module({
-  imports: [],
+  imports: [
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/quiz/graphql/schema.gql',
+    }),
+
+  ]
 })
 export class AppModule {}
