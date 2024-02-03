fa29074355f9d06cbd18df5333f93ebd771d6d86
Ivan Illarionov <s25134@pjwstk.edu.pl>
2024-02-03 15:47:55 +0100

    Imported TypeOrmModule and ConfigModule
    Imported TypeOrmModule for future connection in the service to the database, and ConfigModule for reading the .env file with the database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 
 @Module({
@@ -9,6 +11,19 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/quiz/graphql/schema.gql',
+    }),
+
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: ['dist/quiz/models/*.js'],
+      synchronize: true,
     }),
 
   ]
